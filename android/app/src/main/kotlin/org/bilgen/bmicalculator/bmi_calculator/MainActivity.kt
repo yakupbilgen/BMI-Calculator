cff1bbe2f4841bb14f6f62f599fe59babb0912a5
yakupbilgen <yakupbilgen92@gmail.com>
2024-07-20 23:32:00 +0200

    create project

---
+++ android/app/src/main/kotlin/org/bilgen/bmicalculator/bmi_calculator/MainActivity.kt
@@ -0,0 +1,5 @@
+package org.bilgen.bmicalculator.bmi_calculator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:bmi_calculator/screen/homepage.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      darkTheme: ThemeData.dark(
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
